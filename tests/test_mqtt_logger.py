c8d2670d29fa137866b3c41ad00bc77970a293e7
Blake Haydon <blake.a.haydon@gmail.com>
2022-04-02 16:29:53 +1100

    feat: add additional tests
    - update version to v0.2.5

--- tests/test_mqtt_logger.py
+++ tests/test_mqtt_logger.py
@@ -1,5 +1,29 @@
-from mqtt_logger import __version__
+import os
+import pytest
+
+import mqtt_logger
+
+# FIXME: Use fake broker to test, not a public one
+TEST_BROKER_ADDRESS = "broker.hivemq.com"
 
 
 def test_version():
-    assert __version__ == "0.2.4"
+    assert mqtt_logger.__version__ == "0.2.5"
+
+
+def test_basic_instantiation(tmp_path):
+    mqtt_logger.Recorder(
+        sqlite_database_path=os.path.join(tmp_path, "MQTT_log.db"),
+        topics=["test/#"],
+        broker_address=TEST_BROKER_ADDRESS,
+    )
+
+    mqtt_logger.Playback(
+        sqlite_database_path=os.path.join(tmp_path, "MQTT_log.db"),
+        broker_address=TEST_BROKER_ADDRESS,
+    )
+
+
+def test_playback_without_database(tmp_path):
+    with pytest.raises(Exception):
+        mqtt_logger.Playback(sqlite_database_path=os.path.join(tmp_path, "MQTT_log.db"))
