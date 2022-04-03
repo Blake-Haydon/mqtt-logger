12fad9a551f0f955b819dd16ea23905c45908756
Blake Haydon <blake.a.haydon@gmail.com>
2022-04-03 11:54:43 +1100

    fix: github action for CICD publishing

--- tests/test_mqtt_logger.py
+++ tests/test_mqtt_logger.py
@@ -8,7 +8,7 @@ TEST_BROKER_ADDRESS = "broker.hivemq.com"
 
 
 def test_version():
-    assert mqtt_logger.__version__ == "0.3.0"
+    assert mqtt_logger.__version__ == "0.3.1"
 
 
 def test_basic_instantiation(tmp_path):
