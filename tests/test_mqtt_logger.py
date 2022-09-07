f0a1e7b617550a97901c0daf8b81bae30cf3e6cd
Blake Haydon <blake.a.haydon@gmail.com>
2022-09-07 20:11:42 +1000

    refactor: update to v0.3.2

--- tests/test_mqtt_logger.py
+++ tests/test_mqtt_logger.py
@@ -8,7 +8,7 @@ TEST_BROKER_ADDRESS = "broker.hivemq.com"
 
 
 def test_version():
-    assert mqtt_logger.__version__ == "0.3.1"
+    assert mqtt_logger.__version__ == "0.3.2"
 
 
 def test_basic_instantiation(tmp_path):
